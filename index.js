9eaac3382a56e312f3df0edcc5be62c6c397b1e4
Anshuman Srivastava <73851357+ravenclaw-10@users.noreply.github.com>
2021-11-16 02:33:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-//hello
+document.addEventListener("keypress",function(e){
+    const audio=document.querySelector(`audio[data-key="${e.key}"]`);
+    const key=document.querySelector(`.key[data-key="${e.key}"]`);
+    if (!audio) return; 
+    audio.currentTime=0;
+    audio.play();
+    key.classList.add("playing");
+
+    function removeTransition(e){
+        console.log(e);
+        if (e.propertyName !== "transform") return;
+        this.classList.remove("playing");
+    }
+    const keys=document.querySelectorAll(".key");
+    for(var i=0;i<keys.length;i++){
+        console.log(keys[i]);
+        keys[i].addEventListener("transitionend",removeTransition);
+    }
+
+
+});
